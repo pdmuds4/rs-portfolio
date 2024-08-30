20f2e2ea798cc260be2488190c9ebc16c4ed0969
pdmuds4 <ryosuke3191@gmail.com>
2024-08-30 18:59:04 +0900

    TextのUIコンポーネントを実装

---
+++ src/components/ui/Text/style.ts
@@ -0,0 +1,7 @@
+import { SxProps } from "@mui/material";
+
+export const base_style: SxProps = {
+    opacity: 0.7,
+    fontSize: {xs: "1rem", md: "1.2rem"},
+    lineHeight: {xs: "1.5", md: "1.7"},
+}
