2beb4bb11604d27f8ba71844d32fb317bc3356c2
pdmuds4 <ryosuke3191@gmail.com>
2024-08-30 19:19:15 +0900

    HiddenDividerのUIコンポーネントを実装

---
+++ src/components/ui/HiddenDivider/style.ts
@@ -0,0 +1,6 @@
+import { SxProps } from "@mui/material";
+
+export const body: SxProps = {
+    opacity: 0,
+    margin: "1rem 0"
+}
--- src/components/ui/index.tsx
+++ src/components/ui/index.tsx
@@ -1,3 +1,4 @@
 export { default as Heading } from "./Heading";
 export { default as Section } from "./Section";
 export { default as Text } from "./Text";
+export { default as HiddenDivider } from "./HiddenDivider";
