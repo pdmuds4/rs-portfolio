b27185a06851a271fb5df270ebe05feca61ea74a
pdmuds4 <ryosuke3191@gmail.com>
2024-09-10 14:57:33 +0900

    各Entityを実装

---
+++ src/models/entity/qiita.ts
@@ -0,0 +1,10 @@
+export interface QiitaAnalyzeDTO {
+    tech: string;
+    precentage: number;
+}
+
+export interface QiitaArticleDTO {
+    title: string;
+    link: string;
+    written: Date;
+}
