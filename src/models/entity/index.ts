51975b0f604f0fd649691721d40db3d19e8804ae
pdmuds4 <ryosuke3191@gmail.com>
2024-09-11 03:33:36 +0900

    サービスにindexを追加

--- src/models/entity/index.ts
+++ src/models/entity/index.ts
@@ -2,4 +2,3 @@ export {default as HobbyLikesEntity} from "@models/entity/hobbylikes";
 export {default as SkillsEntity} from "@models/entity/skills";
 export {default as WorksEntity} from "@models/entity/works";
 export {default as ComposeEntity} from "@models/entity/compose";
-export type {QiitaAnalyzeDTO, QiitaArticleDTO} from "@models/entity/qiita";
