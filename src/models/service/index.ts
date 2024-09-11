eafc04a3f8dccc0d41e5aa040d2d2298f2d36486
pdmuds4 <ryosuke3191@gmail.com>
2024-09-12 03:42:55 +0900

    URLの著名サービスを追加

--- src/models/service/index.ts
+++ src/models/service/index.ts
@@ -1,2 +1,5 @@
 export { default as ImageUrlCheckService } from './imageUrlCheck';
 export { default as AudioUrlCheckService } from './audioUrlCheck';
+export { default as SignFileSrcService } from './signFileSrc';
+export { default as UploadFileService } from './uploadFile';
+export { default as DeleteFileService } from './deleteFile';
