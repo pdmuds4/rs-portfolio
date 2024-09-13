d9c1a8ac9bdf958fc84ccb4f5910d5f886e632b4
pdmuds4 <ryosuke3191@gmail.com>
2024-09-14 04:39:09 +0900

    S3関連をservice->usecaseに移動

--- src/models/service/index.ts
+++ src/models/service/index.ts
@@ -1,3 +1,2 @@
 export { default as ImageUrlCheckService } from './imageUrlCheck';
 export { default as AudioUrlCheckService } from './audioUrlCheck';
-export { default as UploadToS3Service } from './uploadToS3';
